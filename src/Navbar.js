4f8c37fa541ce7fa9aa6a7b001b7044d647616a7
haroba2020 <fredriktheyerdahl@gmail.com>
2023-01-26 17:19:17 +0100

    adding first touches to github for desktop editing

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,6 @@
 const Navbar = () => {
     return (  
-        <nav className="nav">
+        <nav className="navbar">
             <h1>The haroba blog</h1>
             <div className="links">
                 <a href="/">Home</a>
