c7900aea2c3ed4c5df959da00960a8fb1febbd4d
Fredrik <fredriktheyerdahl@gmail.com>
2023-01-27 07:47:15 +0100

    moving files to git from desktop

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import  BlogList  from "./BlogList"
 const Home = () => {
     const [blogs, setBlogs] = useState([
@@ -6,15 +6,23 @@ const Home = () => {
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
       ])
+    
+    const [name, setName] = useState('mario')
 
-      const handleDelete = (id) =>{
-        const newBlogs = blogs.filter(blog => blog.id !== id)
-        setBlogs(newBlogs)
-      }
+    const handleDelete = (id) =>{
+      const newBlogs = blogs.filter(blog => blog.id !== id)
+      setBlogs(newBlogs)
+    }
+
+    useEffect(()=>{
+      console.log(blogs)
+    },[name])
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All blogs"/>
+            <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>
+            <button onClick={()=> setName('luigi')}>Change name</button>
+            
         </div>
      );
 }
